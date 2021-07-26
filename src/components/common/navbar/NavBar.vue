1d9e0ca39745630ec3d6388029327fd1b1429eef
siifan168 <1546686828@qq.com>
2021-07-26 16:26:53 +0800

    '第一次提交'

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="nav-bar">
+    <div class="nav-left"><slot name="left"></slot></div>
+    <div class="nav-center"><slot name="center">标题</slot></div>
+    <div class="nav-right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "NavBar"
+	}
+</script>
+
+<style scoped>
+  #nav-bar {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 10;
+
+    display: flex;
+
+    height: 44px;
+    line-height: 44px;
+    text-align: center;
+
+    /*border-bottom: 1px solid #eee;*/
+    box-shadow: 0 1px 1px rgba(100,100,100,.08);
+  }
+
+  .nav-left {
+    width: 10%;
+    margin-top: 5px;
+  }
+
+  .nav-center {
+    flex: 1;
+  }
+
+  .nav-right {
+    margin-top: 5px;
+    width: 10%;
+  }
+</style>
